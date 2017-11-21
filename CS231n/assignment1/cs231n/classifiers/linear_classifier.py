c755339b99d5a00bf6c155f4e2b18a7411692198
Lohrasp <lohraspco@gmail.com>
2017-11-21 14:21:37 -0600

    it is for google cloud

--- CS231n/assignment1/cs231n/classifiers/linear_classifier.py
+++ CS231n/assignment1/cs231n/classifiers/linear_classifier.py
@@ -140,5 +140,6 @@ class Softmax(LinearClassifier):
   """ A subclass that uses the Softmax + Cross-entropy loss function """
 
   def loss(self, X_batch, y_batch, reg):
+
     return softmax_loss_vectorized(self.W, X_batch, y_batch, reg)
 
